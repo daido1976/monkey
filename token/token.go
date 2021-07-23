c04255e57a92c5832a8508f6c3b1aceeb1bd02bd
daido1976 <daido1976@gmail.com>
2021-07-23 16:19:05 +0900

    Support new keywords token in Lexer

--- token/token.go
+++ token/token.go
@@ -38,11 +38,21 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+	"true":   TRUE,
+	"false":  FALSE,
 }
 
 func LookupIdentifier(identifier string) TokenType {
