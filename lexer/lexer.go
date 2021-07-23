dc60cae6a01a144b40844f20a5b272f681650956
daido1976 <daido1976@gmail.com>
2021-07-23 17:11:09 +0900

    Support 2 chars token in Lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -24,7 +24,14 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.char {
 	case '=':
-		tok = newToken(token.ASSIGN, l.char)
+		if l.peekChar() == '=' {
+			prevChar := l.char
+			l.readChar()
+			literal := string(prevChar) + string(l.char)
+			tok = token.Token{Type: token.EQ, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, l.char)
+		}
 	case ';':
 		tok = newToken(token.SEMICOLON, l.char)
 	case '(':
@@ -46,7 +53,14 @@ func (l *Lexer) NextToken() token.Token {
 	case '/':
 		tok = newToken(token.SLASH, l.char)
 	case '!':
-		tok = newToken(token.BANG, l.char)
+		if l.peekChar() == '=' {
+			prevChar := l.char
+			l.readChar()
+			literal := string(prevChar) + string(l.char)
+			tok = token.Token{Type: token.NOT_EQ, Literal: literal}
+		} else {
+			tok = newToken(token.BANG, l.char)
+		}
 	case '<':
 		tok = newToken(token.LT, l.char)
 	case '>':
@@ -89,6 +103,14 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
 
 func (l *Lexer) readIdentifier() string {
