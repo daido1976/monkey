338abdae6a77c5730bb702bef00fdd1f8dd76c5a
daido1976 <daido1976@gmail.com>
2021-07-25 18:07:44 +0900

    Prepare to impl parser

---
+++ parser/parser.go
@@ -0,0 +1,30 @@
+package parser
+
+import (
+	"github.com/daido1976/monkey/ast"
+	"github.com/daido1976/monkey/lexer"
+	"github.com/daido1976/monkey/token"
+)
+
+type Parser struct {
+	l            *lexer.Lexer
+	currentToken token.Token
+	peekToken    token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currentToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
